6daf5123c8db9ec4288ccfc28beb8d2db151e82c
NuTsyTo <maxence.degaugue@gmail.com>
2023-07-05 16:23:12 +0200

    Mise à jour Exercices

--- Mariadb/exercice-02.sql
+++ Mariadb/exercice-02.sql
@@ -26,6 +26,11 @@ WHERE id = 6
 -- - nom : SASS
 -- - description : aucune
 
+INSERT INTO tag
+(name, description)
+VALUES
+('sass', NULL)
+
 -- Exo 2.2
 -- Insérez une school year :
 -- - nom: Promo 2023
@@ -33,38 +38,61 @@ WHERE id = 6
 -- - date de début : 15 mars 2023
 -- - date de fin : aucune
 
+INSERT INTO school_year
+(name, description, start_date, end_date)
+VALUES
+('Promo 2023', 'La promo de l''année 2023', '2023-03-15', NULL)
+
 -- Exo 2.3
 -- Créez une relation entre un student et un tag :
 -- - student : Toto (id 1)
 -- - tag : SASS (id inconnu)
 -- Note : c'est à vous de retrouver l'id du tag mais vous pouvez l'inscrire en dur dans la requête.
 
+
+
 -- Exo 2.4
 -- Mettez à jour le tag `HTML` :
 -- description : Le HTML est un language de structuration de données.
+
+UPDATE tag
+SET description = 'Le HTML est un language de structuration de données.'
+WHERE id = 1
 
 -- Exo 2.5
 -- Mettez à jour le tag `Bash` :
 -- nom : Terminal Bash
 -- description : Le terminal Bash est l'interface en ligne de commande par défaut sous Linux.
 
+UPDATE tag 
+SET name = 'Terminal Bash', description = 'Le terminal Bash est l''interface en ligne de commande par défaut sous Linux.' 
+WHERE name = 'Bash';
+
 -- Exo 2.6
 -- Supprimez la relation entre un student et un tag :
 -- - student : Zacharie Evrard
 -- - tag : Terminal Bash
+
+
 
 -- Exo 2.7
 -- Supprimez la relation entre un student et un project :
 -- - student : Arthur Lacombe
 -- - project : Dicta quia at qui
 
+
+
 -- Exo 2.8
 -- Supprimez toutes les relations entre un student et ses tags :
 -- - student : Odette Thomas
 
+
+
 -- Exo 2.9
 -- Supprimez un student :
 -- - student : Odette Thomas
+
+
 
 -- Exo 2.10
 -- Supprimez un project et toutes les relations avec ses dépendances, c-à-d les relations avec des student et les relations avec des tags :
