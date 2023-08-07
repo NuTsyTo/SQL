8cae65294110a68d36a0d19607a999a65ba365cc
NuTsyTo <135851698+NuTsyTo@users.noreply.github.com>
2023-08-07 11:27:36 +0200

    Update exercice-05.sql

--- Mariadb/exercice-05.sql
+++ Mariadb/exercice-05.sql
@@ -4,12 +4,17 @@
 -- Exo 5.1
 -- Listez tous les students avec leurs tags
 
-
+SELECT student.firstname, tag.name
+FROM student
+LEFT OUTER JOIN student_tag ON student.id = student_tag.student_id
+LEFT OUTER JOIN tag ON student_tag.student_id = tag.id;
 
 -- Exo 5.2
 -- Listez tous les tags avec leurs students
 
-
+SELECT tag.name, student_id
+FROM tag
+LEFT OUTER JOIN student_tag ON tag.id = student_tag.tag_id;
 
 -- Exo 5.3
 -- Listez le student dont l'id est `2` avec ses tags
